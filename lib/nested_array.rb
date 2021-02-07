787c0069bf4e2b79227c71b835320f6b6c9b1ee6
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-07 21:26:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,7 +38,7 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
 
 def matrix_lookup(matrix, row, column)
- matrix_lookup = [
+  matrix_lookup = [
    assembled_matrix[1][1]
    ]
  end
